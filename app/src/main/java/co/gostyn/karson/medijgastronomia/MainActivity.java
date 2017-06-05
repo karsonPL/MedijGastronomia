c789346ab1453420a47fb42b494a8296eb80423d
karsonPL <karson@gostyn.co>
2017-06-05 15:43:56 +0200

    2017.06.05p

--- app/src/main/java/co/gostyn/karson/medijgastronomia/MainActivity.java
+++ app/src/main/java/co/gostyn/karson/medijgastronomia/MainActivity.java
@@ -1,15 +1,39 @@
 package co.gostyn.karson.medijgastronomia;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.button_medij)
+    ImageView buttonMedij;
+    @Bind(R.id.button2)
+    Button button2;
+    @Bind(R.id.button1)
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
 
+    @OnClick({R.id.button2, R.id.button1})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.button2:
+                break;
+            case R.id.button1:
+                break;
+        }
+    }
 }
